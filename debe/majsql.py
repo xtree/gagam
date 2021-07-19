6f1b5fe72bad554a1cf74874323e32698c47d76e
tree <xtree0@gmail.com>
2021-07-19 15:50:30 +0200

    try mysql connector

---
+++ debe/majsql.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="10.4.0.238",
+  user="tnt",
+  password="tnt",
+  database="tnt"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM users")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
